1d3e88df498ce6b1798e39d5b137e4c2dcc52ce0
Abel Ianovici <ianovici.abel04@gmail.com>
2025-01-10 00:12:34 +0100

    Added interface for engine

--- src/engine.c
+++ src/engine.c
@@ -1,6 +1,19 @@
 #include "engine.h"
 
+void selectRenderType(renderType type) { RENDER_TYPE = type; }
+
+void enableMouse() { mouse = true; }
+
+void setup() {
+  // SETUP ENGINE MODULES AND ENV
+}
+
 int run() {
-  
+  running = true;
+  while (running) {
+    // DO STUFF
+    running = false;
+  }
   return 0;
 }
+
--- src/engine.h
+++ src/engine.h
@@ -8,16 +8,31 @@ typedef enum {
   Continious
 } renderType;
 
-extern const renderType RENDER_TYPE;
+extern renderType RENDER_TYPE;
 
 extern bool running;
 extern bool mouse;
 
-void selectRenderType(renderType);
+/*
+ * Select the type of render
+ * @param type: the type to select (Static, Continious)
+ */
+void selectRenderType(renderType type);
+
+/*
+ * Enable the mouse to be used
+ */
 void enableMouse();
 
+/*
+ * Setup Engine modules, and environment
+ */
 void setup();
 
+/*
+ * Start game loop
+ * @return: an int status code
+ */
 int run();
 
 #endif
